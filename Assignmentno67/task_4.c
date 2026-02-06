8820eb3e970822ead09d78bf8a12597e10057d06
Pratik_Dhayagude <pratikdhaygude21@gmail.com>
2026-02-06 09:12:54 +0530

    Add new

---
+++ Assignmentno67/task_4.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+
+typedef unsigned int UINT;
+
+UINT OffBit(UINT iValue)
+{
+    UINT iMask = 1<<6|1<<9;
+    iValue = iValue ^ iMask;
+    return iValue;
+
+
+}
+int main()
+{
+
+    UINT iNo = 0;
+    UINT iRet = 0;
+    printf("Enter the number:\n");
+    scanf("%u",&iNo);
+
+    iRet = OffBit(iNo);
+    printf("Modified number is :%u\n",iRet);
+
+    return 0;
+}
+
---
+++ Assignmentno67/task_5.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+
+typedef unsigned int UINT;
+
+UINT OffBit(UINT iValue)
+{
+    UINT iMask = 0x0F;
+    return (iValue | iMask);
+
+}
+int main()
+{
+
+    UINT iNo = 0;
+    UINT iRet = 0;
+    printf("Enter the number:\n");
+    scanf("%u",&iNo);
+
+    iRet = OffBit(iNo);
+    printf("Modified number is :%u\n",iRet);
+
+    return 0;
+}
+
